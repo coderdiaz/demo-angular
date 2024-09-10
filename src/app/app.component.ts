0292f82bca67407c448c21d6d62782bb7768a1a6
coderdiaz <coderdiaz@gmail.com>
2024-09-10 13:05:25 -0600

    feat: implement components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world.component';
+import { DirectiveExampleComponent } from './directive-example/directive-example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, DirectiveExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
