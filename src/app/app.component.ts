0fc52d8c574f847d3701c93a7ef8270b4bb85fd2
coderdiaz <coderdiaz@gmail.com>
2024-09-11 09:58:42 -0600

    feat: configured inputs and new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world.component';
 import { DirectiveExampleComponent } from './directive-example/directive-example.component';
+import { LifecycleExampleComponent } from './lifecycle-example/lifecycle-example.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloWorldComponent, DirectiveExampleComponent],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    HelloWorldComponent,
+    DirectiveExampleComponent,
+    LifecycleExampleComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-angular';
+  name = '';
+  hiddenLifecycleExample = false;
 }
