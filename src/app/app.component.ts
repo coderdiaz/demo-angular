fb53b981132e44ab58e22d629f86dec34c4bae2e
coderdiaz <coderdiaz@gmail.com>
2024-09-11 11:41:17 -0600

    feat: added logic to handle fetch characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world.component';
 import { DirectiveExampleComponent } from './directive-example/directive-example.component';
 import { LifecycleExampleComponent } from './lifecycle-example/lifecycle-example.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ExampleService } from './services/example.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,8 +22,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-angular';
   name = '';
   hiddenLifecycleExample = false;
+  characters$: Observable<any>;
+
+  constructor(
+    private exampleService: ExampleService,
+  ) {
+    this.characters$ = this.exampleService.example$;
+  }
+
+  ngOnInit() {
+    this.exampleService
+      .getCharacters()
+      .subscribe();
+  }
 }
